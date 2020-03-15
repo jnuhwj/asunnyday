c32f58df9f6f0535904b40560d6d2fe638e23e14
jnuhwj <jlstephy@163.com>
2020-03-15 11:53:01 +0800

    leetcode ready

--- main.c
+++ main.c
@@ -1,7 +1,62 @@
+/*
+ *for leetcode 118 yanghuisanjiao 1
+ *
+*/
 #include <stdio.h>
+#include <stdlib.h>
+
+int** generate(int numRows, int* returnSize, int** returnColumnSizes){
+    int i = 0;
+    int j = 0;
+    int **new_array = NULL;
+
+    if (numRows == 0) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    new_array = (int **)malloc(numRows * sizeof(int *));
+    *returnColumnSizes = (int *)malloc(numRows * sizeof(int));
+    *returnSize = numRows;
+    for (i = 0; i < numRows; i++) {
+        (*returnColumnSizes)[i] = i + 1;
+        new_array[i] = (int *)malloc(sizeof(int) * (i + 1));
+        for (j = 0; j < (*returnColumnSizes)[i]; j++) {
+            if (j == 0 || j == (*returnColumnSizes)[i] - 1) {
+                new_array[i][j] = 1;
+                continue;
+            }
+            new_array[i][j] = new_array[i - 1][j - 1] + new_array[i - 1][j];
+        }
+    }
+
+    return new_array;
+}
 
 int main()
 {
-    printf("a sunny day\n");
-    return 0;
+    int ret = 0;
+    int i = 0;
+    int j = 0;
+    int return_size = 0;
+    int *returnColumnSizes = NULL;
+    int **new_array = NULL;
+    printf("Hello world!\n");
+
+    new_array = generate(10, &return_size, &returnColumnSizes);
+
+    for (i = 0; i < return_size; i++) {
+        for (j = 0; j < returnColumnSizes[i]; j++) {
+            printf("%d ", new_array[i][j]);
+        }
+        printf("\n");
+    }
+
+    free(returnColumnSizes);
+    for (i = 0; i < return_size; i++) {
+        free(new_array[i]);
+    }
+
+    return ret;
 }
+
